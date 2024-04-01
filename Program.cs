6743263047cde798b4f7dbb08ad4318ef8b39bc4
Simon Isupov <is_se@mail.ru>
2024-04-01 21:41:18 +0300

    2

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 2: Задайте строку, содержащую латинские буквы в обоих регистрах. 
+// Сформируйте строку, в которой все заглавные буквы заменены на строчные.
+
+string GetUnderScore(string text)
+{
+    string target = "";
+
+    for (int i =0; i<text.Lenght; i++)
+    {
+        if (char.IsUpper(i))
+        {
+            text += "_" + char.ToLower(c);
+        }
+        else
+        {
+            text += i;
+        }
+    }
+    return target;
+}
+string name = new string (PotyretskA);
+GetUnderScore(name);
+System.Console.WriteLine(name);
